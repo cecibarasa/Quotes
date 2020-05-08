2603e874f0b6551ef5c92dec2a88b440456919bc
Cecilia Barasa <cecibarasa@gmail.com>
2020-05-08 13:33:52 +0300

    add highlight color

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,14 +1,20 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(private elem:ElementRef) { 
-    this.elem.nativeElement.style.textDecorationColor='red';
+  constructor(private elem:ElementRef) { }
+  @Input() highlightColor: string;
+
+  @HostListener('click') onClick(){
+    this.textDeco('highlight')
   }
 
+  private textDeco(highlightColor) {
+    this.elem.nativeElement.style.background = highlightColor
+    
   }
 
 }
