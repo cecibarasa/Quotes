f427345bca472ba9fc3e94f231abe950ebb759dc
Cecilia Barasa <cecibarasa@gmail.com>
2020-05-08 11:31:19 +0300

    add date

--- src/app/upvote.directive.ts
+++ src/app/upvote.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appUpvote]'
 })
 export class UpvoteDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { }
+
+  
 
 }
